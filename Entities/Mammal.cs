074988b4409dca8805855650062b5eff6cabc0a0
Derek Peacock <derek.peacock@yahoo.com>
2024-07-16 17:04:53 +0100

    Added the mammal entity

---
+++ Entities/Mammal.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorAppEduardo.Entities
+{
+    public class Mammal
+    {
+        public int Id { get; set; }
+        public string ScientificName { get; set; }
+
+        public string CommonName { get; set; }
+
+    }
+}
